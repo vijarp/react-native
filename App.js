833424234e2ced5e12bc1328ae1ceee3d9fabbeb
vijarp <vijarp1@gmail.com>
2020-04-18 22:10:15 +0530

    Calculator Code

--- App.js
+++ App.js
@@ -1,114 +1,208 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React,{Component,Fragment} from 'react';
+import CalculatorResponse from './components/CalculatorResponse';
+import CalculatorButtonsContainer from './components/CalculatorButtonsContainer';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+    AppRegistry,
+    PermissionsAndroid,
+    TextInput,
+    TouchableHighlight,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    FlatList,
+    Platform,
+    Button
+  } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+class Calculator extends React.Component {
+    constructor() {
+      super();
+  
+      this.state = {
+        first: '0',
+        second: '',
+        operator: '',
+        result: 0,
+        isResult: false,
+      };
+  
+      this.refresh = this.refresh.bind(this);
+      this.handleButtonPress = this.handleButtonPress.bind(this);
+      this.getResult = this.getResult.bind(this);
+    }
+  
+    getResult() {
+      const { first, second, operator } = this.state;
+  
+      const parsedFirst = parseFloat(first);
+      const parsedSecond = parseFloat(second) || 0;
+      let result = 0;
+  
+      switch (operator) {
+        case '+':
+          result = parsedFirst + parsedSecond;
+          break;
+        case '−':
+          result = parsedFirst - parsedSecond;
+          break;
+        case '×':
+          result = parsedFirst * parsedSecond;
+          break;
+        case '÷':
+          if (!parsedSecond || parsedSecond === 0) {
+            result = 'Error';
+          } else {
+            result = parseFloat(parsedFirst / parsedSecond).toFixed(8);
+          }
+  
+          break;
+        default:
+          // console.log('wrong operator');
+      }
+  
+      this.setState({
+        result,
+        isResult: true,
+      });
+    }
+  
+    refresh() {
+      this.setState({
+        first: '0',
+        second: '',
+        operator: '',
+        result: 0,
+      });
+    }
+  
+  
+    handleButtonPress(button) {
+      const { isResult } = this.state;
+      let { first, second, operator } = this.state;
+  
+      switch (button) {
+        case '0':
+          if (!isResult) {
+            if (!operator) {
+              if (first[0] !== '0' || first.length !== 1) {
+                first += '0';
+              }
+            } else if (second[0] !== '0' || second.length !== 1) {
+              second += '0';
+            } else {
+              second = '0';
+            }
+  
+            this.setState({ first, second, operator });
+          } else {
+            this.setState({
+              first: '0',
+              second: '',
+              operator: '',
+              result: 0,
+              isResult: false,
+            });
+          }
+  
+          break;
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          if (!isResult) {
+            if (!operator) {
+              if (first[0] === '0' && first.length === 1) {
+                first = button;
+              } else {
+                first += button;
+              }
+            } else if (second[0] === '0' && second.length === 1) {
+              second = button;
+            } else {
+              second += button;
+            }
+  
+            this.setState({ first, second, operator });
+          } else {
+            this.setState({
+              first: button,
+              second: '',
+              operator: '',
+              result: 0,
+              isResult: false,
+            });
+          }
+  
+          break;
+        case '.':
+          if (!operator) {
+            if (!first.includes('.')) {
+              first += button;
+            }
+          } else if (!second.includes('.')) {
+            second += button;
+          }
+  
+          this.setState({ first, second, operator });
+  
+          break;
+        case '+':
+        case '−':
+        case '×':
+        case '÷':
+          if (!operator) {
+            operator = button;
+  
+            this.setState({ first, second, operator });
+          } else {
+            this.getResult();
+          }
+          break;
+        case '=':
+          this.getResult();
+          break;
+        default:
+            // console.log('wrong operator');
+      }
+    }
+  
+    render() {
+      const { first, second, operator, result } = this.state;
+  
+      return (
+        <View><Text>Vijeet</Text>
+          <CalculatorResponse
+            first={first}
+            second={second}
+            operator={operator}
+            result={result}
+            refresh={this.refresh}
+          />
+  
+          <CalculatorButtonsContainer
+            handleButtonPress={this.handleButtonPress}
+          />
+  
+          <StatusBar barStyle="light-content" />
+        </View>
+      );
+    }
+  }
+  
+  const styles3 = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
+  
+  export default Calculator;
+  
+  
